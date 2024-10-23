94b2a8d05b3b2f79259e976056ccb5dbde6c22ed
manny-dev <manfredcb@gmail.com>
2024-10-23 17:28:19 -0300

    Add main project files

---
+++ Assets/Scripts/SnapZoneFacade.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapZoneFacade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Renderer snapZone = GetComponent<Renderer>();
+        snapZone.material.color = Color.red;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Renderer snapZone = GetComponent<Renderer>();
+        snapZone.material.color = new Color(0, 1, 0, 0.2f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Renderer snapZone = GetComponent<Renderer>();
+        snapZone.material.color = new Color(1, 0, 0, 0.2f);
+    }
+}
