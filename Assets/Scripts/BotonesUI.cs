d2e6267786e27d442e0c9f635054d0033d0607d0
Braian Arguello <braian.arguello@hotmail.com>
2024-11-20 17:17:55 -0300

    Se generaron las teclas para tener acceso al inicio y cerrar el programa desde la simulacion. Tambien se genero el script

---
+++ Assets/Scripts/BotonesUI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Para manejar escenas
+
+public class BotonesUI : MonoBehaviour
+{
+    void Update()
+    {
+        // Si el jugador presiona la tecla 'H', volver al inicio
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            VolverAlInicio();
+        }
+
+        // Si el jugador presiona la tecla 'K', cerrar el programa
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            CerrarPrograma();
+        }
+    }
+
+    // Método para volver al inicio
+    public void VolverAlInicio()
+    {
+        SceneManager.LoadScene("UI-INTERFACES");// Reemplaza "Inicio" con el nombre de tu escena de inicio
+    }
+
+    // Método para cerrar el programa
+    public void CerrarPrograma()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; // Para detener el modo Play en Unity
+        Application.Quit(); // Para cerrar el programa en una build
+
+    }
+}
